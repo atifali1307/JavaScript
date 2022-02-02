58846c54f398f3624ea50f69005e2795a4faec37
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-02-02 15:52:26 +0530

    Add files via upload

---
+++ fun1.js
@@ -0,0 +1,39 @@
+/*function sum()
+{
+  let num1=10 
+  let num2=20
+  let sum=num1+num2
+  console.log("Sum of 2 numbers is= "+sum)
+}
+sum()
+*/
+
+
+/*function sum(num1, num2)
+{
+  let sum=num1+num2
+  console.log("Sum of 2 numbers is= "+sum)
+}
+sum(10,20)
+*/
+
+function sum(num1, num2, type)
+{
+ let sum=num1+num2;
+ console.log("Sum of "+type+" variable is= "+sum)
+}
+sum(10.23,30.31,"float")
+sum(232,50,"integer")
+sum("Atif"," Ali","string")
+
+
+function add(num1,num2)
+{
+  return num1+num2
+}
+var result=add(10.21,31.2)
+console.log("Sum of 2 numbers is= "+result)
+
+
+
+
---
+++ functionusingobject.js
@@ -0,0 +1,14 @@
+var div=function calculation(num1,num2)       //object function
+{
+  console.log("Division of 2 numbers is= "+(num1/num2))
+}
+div(56,7)
+
+var mul=function(num1,num2)     // Anonumous Function (Function without any name)
+{
+    console.log("Multiplication of 2 numbers is= "+(num1*num2))
+}
+mul(20,5)
+
+
+
